0e17561ffb3275ddba19a08dcb136b0bb3cb316b
hamzaayad <60011170+hamzaayad@users.noreply.github.com>
2021-10-07 21:31:41 +0500

    Add files via upload

---
+++ todo-app-practice/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Todo from "./Todo";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo - APP</h1>
+      <Todo title="React" />
+      <Todo title="Node" />
+    </div>
+  );
+}
+
+export default App;
